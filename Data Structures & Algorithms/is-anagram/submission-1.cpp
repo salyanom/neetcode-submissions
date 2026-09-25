03b69c66e5fb9e4473e49ffb131a557775619bed
Om Jagdish Salyan <omjagdishsalian2005@gmail.com>
2026-09-25 22:32:20 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t) {
+            count[c - 'a']--;
+        }
+
+        for (int value : count) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
